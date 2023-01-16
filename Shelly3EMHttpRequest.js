89db3943984d5a6344b6e12be26b300311f25bcd
Andreas Podlich <a-podlich@web.de>
2023-01-16 19:55:37 +0100

    Translate script from german to english (MQ)

--- Shelly3EMHttpRequest.js
+++ Shelly3EMHttpRequest.js
@@ -1,13 +1,13 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Skript zum Abfragen des Status von einem Shelly 3EM um Energie / Leistungsdaten im 5-Sekunden-Takt zu erhalten
+// This Script polls energy data from Shelly 3EM via HTTP-Request and create datapoints in first run in your javascript folder
 // Version 1.0
-// Author Andreas Podlich 
+// Created Andreas Podlich 
 // 22.06.2021
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-var URL = 'http://192.168.10.12';               // Hier IP von Shelly 3EM Eintragen
-var request = 5;                                // Hier Poll-Zeit in Sekunden eingeben
+var URL = 'http://192.168.10.12';               // Put in IP from Shelly 3EM
+var request = 5;                                // Polltime (Seconds)
 
 
 var L1
@@ -18,11 +18,11 @@ var Data = "javascript.0.Leistung.System.data";
 
 
 schedule("*/" + request + " * * * * *", async function () {
-        createState("Leistung.L1");
-        createState("Leistung.L2");
-        createState("Leistung.L3");
-        createState("Leistung.total");
-        createState("Leistung.System.data")
+        createState("Shelly3EM.L1");
+        createState("Shelly3EM.L2");
+        createState("Shelly3EM.L3");
+        createState("Shelly3EM.total");
+        createState("Shelly3EM.System.data")
 
 
   try {                 
@@ -43,9 +43,9 @@ schedule("*/" + request + " * * * * *", async function () {
     L3 = obj.emeters[2].power;
     total = L1 + L2 + L3;
   
-  setState('javascript.0.Leistung.L1', (L1));
-  setState('javascript.0.Leistung.L2', (L2));
-  setState('javascript.0.Leistung.L3', (L3));
-  setState('javascript.0.Leistung.total', (total.toFixed(2)));
+  setState('javascript.0.Shelly3EM.L1', (L1));
+  setState('javascript.0.Shelly3EM.L2', (L2));
+  setState('javascript.0.Shelly3EM.L3', (L3));
+  setState('javascript.0.Shelly3EM.total', (total.toFixed(2)));
    
 });
